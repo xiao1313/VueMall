f469292166e66475b639b6d5d4a177022f034eec
Xiao <231289862@qq.com>
2020-04-18 10:07:17 +0800

    Router

---
+++ src/pages/order.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Order
+        <order-header></order-header>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+<script>
+import OrderHeader from './../components/OrderHead'
+import NavFooter from './../components/NavFooter'
+export default {
+    name:'order',
+    components:{
+        OrderHeader,
+        NavFooter
+    }
+}
+</script>
--- test.js
+++
@@ -1,3 +0,0 @@
-var helloStr;
-var secondStr;
-document.write(helloStr);
